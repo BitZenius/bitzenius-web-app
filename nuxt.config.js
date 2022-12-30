3d60f963222297286b570493f88a9b51cf280aa3
DopeBoi182 <akhmadeka07@gmail.com>
2022-12-30 21:13:21 +0700

    viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     title: 'The best investment tools',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
