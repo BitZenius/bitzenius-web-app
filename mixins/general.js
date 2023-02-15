fb673b28d0de30e7cb1130fa9cad16ecba55fec4
Adi Setiawan <mas.adisetiawan@gmail.com>
2023-02-15 23:16:31 +0700

    Fix: Error menu on production

--- mixins/general.js
+++ mixins/general.js
@@ -1,16 +1,10 @@
 export default {
   computed: {
-    title() {
-      return this.$store.state.title;
-    },
     user() {
       return this.$store.state.authUser;
     },
     subscription() {
       return this.$store.state.subscription;
-    },
-    topMargin() {
-      return this.$store.state.globalCss.topMargin;
-    },
+    }
   },
 }
